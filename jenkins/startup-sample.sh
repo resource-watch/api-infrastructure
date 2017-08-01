d5fd7ad2e1e8e2a8e729384c6f8fff750523f20d
Sergio Gordillo <sgordillogallardo@gmail.com>
2017-08-01 10:58:11 +0200

    mongo, redis and elastic specs for staging

--- jenkins/startup-sample.sh
+++ jenkins/startup-sample.sh
@@ -2,5 +2,3 @@
 
 gcloud auth activate-service-account --key-file=gcloudcredentials.json
 gcloud config set project resource-watch
-gcloud container clusters get-credentials wri-prod \
-    --zone us-east1-c --project resource-watch
