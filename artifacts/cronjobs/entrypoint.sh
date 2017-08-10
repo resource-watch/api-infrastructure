b2a2c6d165824eb337979f87c6a6c37f1352ff89
Sergio Gordillo <sgordillogallardo@gmail.com>
2017-08-10 14:51:12 +0200

    fix: cronjob

--- artifacts/cronjobs/entrypoint.sh
+++ artifacts/cronjobs/entrypoint.sh
@@ -7,6 +7,7 @@ case "$1" in
         echo -e "$GCLOUD_BACKUPS_CREDENTIALS" | base64 -d > /cronjobs/gcloudcredentials.json
         gcloud auth activate-service-account --key-file=/cronjobs/gcloudcredentials.json
         gcloud config set project resource-watch
+        crontab /etc/cron.d/cron
         cron && tail -f /var/log/cron.log
         ;;
     *)
