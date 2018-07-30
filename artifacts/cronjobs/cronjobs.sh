92b5914095ab5e9110135cdc5d2569b910fdef5c
Sergio Gordillo <sgordillogallardo@gmail.com>
2018-07-30 10:37:20 +0200

    neo backups

--- artifacts/cronjobs/cronjobs.sh
+++ artifacts/cronjobs/cronjobs.sh
@@ -9,5 +9,10 @@ echo "start autopostgresbackup"
 # GCLOUD STORAGE BUCKET rsync
 gsutil rsync -r /cronjobs/backups/postgres gs://api-backups/postgres
 rm -rf /cronjobs/backups/postgres/*
+echo "start autoneobackup"
+/cronjobs/autoneobackup.sh | true
+# GCLOUD STORAGE BUCKET rsync
+gsutil rsync -r /cronjobs/backups/neo4j gs://api-backups/neo4j
+rm -rf /cronjobs/backups/neo4j/*
 echo "start autoelasticbackup"
 /cronjobs/autoelasticbackup.sh | true
