eb821a7fb24aa4809461f89fb3a0ae44221e4dd5
Tiago Garcia <tiagojsag@gmail.com>
2019-02-15 14:17:16 +0100

    Add CT mongo backups

--- artifacts/cronjobs/cronjobs.sh
+++ artifacts/cronjobs/cronjobs.sh
@@ -1,18 +1,24 @@
-# MONGO BACKUPS
-echo "start automongobackup"
+#!/usr/bin/env bash
+
+echo "Starting auto mongo backup"
 /cronjobs/automongobackup.sh | true
-# GCLOUD STORAGE BUCKET rsync
 gsutil rsync -r /cronjobs/backups/mongo gs://api-backups/mongo
 rm -rf /cronjobs/backups/mongo/*
-echo "start autopostgresbackup"
+
+echo "Starting auto mongo-ct backup"
+/cronjobs/automongoctbackup.sh | true
+gsutil rsync -r /cronjobs/backups/mongo-ct gs://api-backups/mongo-ct
+rm -rf /cronjobs/backups/mongo-ct/*
+
+echo "Starting auto postgres backup"
 /cronjobs/autopostgresbackup.sh | true
-# GCLOUD STORAGE BUCKET rsync
 gsutil rsync -r /cronjobs/backups/postgres gs://api-backups/postgres
 rm -rf /cronjobs/backups/postgres/*
-echo "start autoneobackup"
+
+echo "Starting auto neo backup"
 /cronjobs/autoneobackup.sh | true
-# GCLOUD STORAGE BUCKET rsync
 gsutil rsync -r /cronjobs/backups/neo4j gs://api-backups/neo4j
 rm -rf /cronjobs/backups/neo4j/*
-echo "start autoelasticbackup"
+
+echo "Starting auto elastic backup"
 /cronjobs/autoelasticbackup.sh | true
