2c5b305333f5b788afab14390f29e928d8bc8dfa
Thomas Maschler <tmaschler@wri.org>
2020-01-15 16:13:29 -0500

    Format

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -6,57 +6,57 @@ output "environment" {
 }
 
 output "tags" {
-  value = local.tags
+  value       = local.tags
   description = "Default tags which should be assigned to all resources"
 }
 
 
 output "account_id" {
-  value = data.aws_caller_identity.current.account_id
+  value       = data.aws_caller_identity.current.account_id
   description = "ID of AWS account"
 }
 
 output "vpc_id" {
-  value = module.vpc.id
+  value       = module.vpc.id
   description = "ID of VPC"
 }
 
 output "public_subnet_ids" {
-  value = module.vpc.public_subnet_ids
+  value       = module.vpc.public_subnet_ids
   description = "List of all public subnet IDs"
 }
 
 output "private_subnet_ids" {
-  value = module.vpc.private_subnet_ids
+  value       = module.vpc.private_subnet_ids
   description = "List of all private subnet IDs"
 }
 
 output "nat_gateway_ips" {
-  value = module.vpc.nat_gateway_ips
+  value       = module.vpc.nat_gateway_ips
   description = "List of all NAT Gateway IPs"
 }
 
 output "bastion_hostname" {
-  value = module.vpc.bastion_hostname
+  value       = module.vpc.bastion_hostname
   description = "Hostname of bastion host for VPC"
 }
 
 output "cidr_block" {
-  value = module.vpc.cidr_block
+  value       = module.vpc.cidr_block
   description = "CIDR for VPC"
 }
 
 output "default_security_group_id" {
-  value = aws_security_group.default.id
+  value       = aws_security_group.default.id
   description = "ID of default security group"
 }
 
 output "key_pairs" {
-  value = values(aws_key_pair.all)[*].key_name
+  value       = values(aws_key_pair.all)[*].key_name
   description = "List of available key pairs"
 }
 
 output "authorized_keys_ec2_user" {
-  value = data.template_file.authorized_keys_ec2_user.rendered
+  value       = data.template_file.authorized_keys_ec2_user.rendered
   description = "User data script to add keypairs to authorized_key file on EC2 instance"
 }
