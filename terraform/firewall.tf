641e16f7d576e3f173b297b89d27407c4945b3d2
Tiago Garcia <tiagojsag@gmail.com>
2020-01-16 12:46:13 +0100

    Add support for configurable CIDR for bastion ingress SG

--- terraform/firewall.tf
+++ terraform/firewall.tf
@@ -18,7 +18,7 @@ resource "aws_security_group_rule" "default_ssh_ingress" {
   from_port         = 22
   to_port           = 22
   protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
+  cidr_blocks       = [var.ingress_allow_cidr_block]
   security_group_id = aws_security_group.default.id
 }
 
--- terraform/vars/core-staging.tfvars
+++ terraform/vars/core-staging.tfvars
@@ -1,1 +1,2 @@
 environment = "staging"
+ingress_allow_cidr_block = "216.70.220.184/32" # WRI Office
