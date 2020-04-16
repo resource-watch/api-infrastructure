10e4a74ac2d4bb86fe717aa83e174c5ab0f8f85e
Tiago Garcia <tiagojsag@gmail.com>
2020-04-16 11:47:27 +0200

    Document certificate steps prior to ingress deployment

--- k8s-aws/bootstrap.sh
+++ k8s-aws/bootstrap.sh
@@ -27,6 +27,12 @@ kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/aws-alb-ingre
 kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/aws-alb-ingress-controller/v1.1.4/docs/examples/alb-ingress-controller.yaml
 ## Next, you MUST configure the ingress controller. See https://docs.aws.amazon.com/eks/latest/userguide/alb-ingress.html for details
 
+#
+# At this stage, you should go into AWS ACM and provision the certificates associated with the domains captured in your ingresses
+# The ALB Ingress controller will automagically associate certificates by domain, but only if they already exist.
+# Also: be sure to use a cert per ingress.
+#
+
 
 #
 # Ingress
@@ -75,8 +81,3 @@ helm install mongodb-apps stable/mongodb-replicaset -f mongodb/mongo-apps-values
 # See also: https://docs.aws.amazon.com/AmazonCloudWatch/latest/monitoring/deploy-container-insights-EKS.html
 #
 curl https://raw.githubusercontent.com/aws-samples/amazon-cloudwatch-container-insights/latest/k8s-deployment-manifest-templates/deployment-mode/daemonset/container-insights-monitoring/quickstart/cwagent-fluentd-quickstart.yaml | sed "s/{{cluster_name}}/core-k8s-cluster/;s/{{region_name}}/us-east-1/" | kubectl apply -f -
-
-#
-# Once everything is up and running, you need to manually provision the SSL certificates on AWS ACM, using DNS validation.
-# Once provisioned and validated the ALB Ingress Controller should pick them up automagically (you may need to delete + re-apply the ingresses)
-#
