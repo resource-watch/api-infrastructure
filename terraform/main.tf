5d5330cb52072866f6050a7c040bac2fd9a12a18
Thomas Maschler <tmaschler@wri.org>
2020-09-17 23:58:58 -0400

    add bastion host to documentdb security group

--- terraform/main.tf
+++ terraform/main.tf
@@ -32,7 +32,7 @@ module "vpc" {
     "kubernetes.io/cluster/${lower(replace(local.project, " ", "-"))}-k8s-cluster-${var.environment}" : "shared"
     "kubernetes.io/role/elb" : 1
   }
-  security_group_ids = [aws_security_group.default.id, module.postgresql.security_group_id]
+  security_group_ids = [aws_security_group.default.id, module.postgresql.security_group_id, module.documentdb.security_group_id]
 }
 
 # Create a k8s cluster using AWS EKS
