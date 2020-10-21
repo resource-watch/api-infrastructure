1fb674bc92597f1a87d8051c587b4710478e067b
Ethan Roday <ethan.roday@gmail.com>
2020-10-21 06:19:01 -0700

    Switch to no-poll approach

--- lambda/eks_scaling/src/eks_scaling.py
+++ lambda/eks_scaling/src/eks_scaling.py
@@ -4,80 +4,66 @@ import json
 import boto3
 
 CLUSTER_NAME = 'core-k8s-cluster-dev'
-NODE_GROUPS_TO_SCALE = [
-    {
-        'nodegroup_name': 'apps-node-group',
+NODEGROUPS_TO_SCALE = {
+    'apps-node-group': {
         'scaling_config': {
             'minSize': 1,
             'maxSize': 16,
             'desiredSize': 3
         }
     },
-    {
-        'nodegroup_name': 'gfw-node-group',
+    'gfw-node-group': {
         'scaling_config': {
             'minSize': 1,
             'maxSize': 4,
             'desiredSize': 1
         }
     }
-]
+}
 
-async def wait_for_update(client, nodegroup_name, initial_progress, update_id):
-    progress = initial_progress
-    response = None
-    while (progress == 'InProgress'):
-        await asyncio.sleep(5)
-        response = client.describe_update(
-            name=CLUSTER_NAME,
-            nodegroupName=nodegroup_name,
-            updateId=update_id
-        )
-        progress = response['update']['status']
-        print('Progress:', nodegroup_name, progress)
-    return response, progress
-
-
-async def update_nodegroup_config(eks_client, nodegroup_name, scaling_config):
-
-    # Update the nodegroup with the desired config
-    response = eks_client.update_nodegroup_config(
+def should_update_scaling_config(client, nodegroup_name, desired_scaling_config):
+    response = client.describe_nodegroup(
         clusterName=CLUSTER_NAME,
-        nodegroupName=nodegroup_name,
-        scalingConfig=scaling_config
+        nodegroupName=nodegroup_name
     )
-    
-    # Wait for the update to report success or failure
-    initial_progress = response['update']['status']
-    update_id = response['update']['id']
-    final_response, final_progress = await wait_for_update(eks_client, nodegroup_name, initial_progress, update_id)
+    nodegroup = response['nodegroup']
 
-    if final_progress != 'Successful':
-        if (final_response is not None):
-            errors = final_response['errors']
-            raise Exception('Update failed with errors: {}'.format(json.dumps(errors, indent=2, default=str)))
-        else:
-            raise Exception('Update failed for unknown reasons. Response was empty.')
+    # TODO: how to handle this on the backend? At scaledown, need to force the update regardless of current status
+    # Don't update if node group is already updating (or down) or if the config is already correct
+    if nodegroup['status'] != "ACTIVE":
+        print("Nodegroup {} has status {}. Skipping update.".format(nodegroup_name, nodegroup['status']))
+        return False
+    elif nodegroup['scalingConfig'] == desired_scaling_config:
+        print("Nodegroup {} already has desired scaling config {}. Skipping update.".format(
+            nodegroup_name, json.dumps(nodegroup['scalingConfig'])
+        ))
+        return False
+    print("Proceeding with update for nodegroup {}.".format(nodegroup_name))
+    return True
 
-    return final_response
-
-async def scale_nodegroups():
+def scale_nodegroups():
     eks_client = boto3.client('eks')
-    responses = []
-    for node_group in NODE_GROUPS_TO_SCALE:
-        # TODO: Make these run in parallel
-        responses.append(
-            await update_nodegroup_config(eks_client, node_group['nodegroup_name'], node_group['scaling_config'])
-        )
-    return responses
+    for nodegroup_name in NODEGROUPS_TO_SCALE:
+        scaling_config = NODEGROUPS_TO_SCALE[nodegroup_name]['scaling_config']
+        if should_update_scaling_config(eks_client, nodegroup_name, scaling_config):
+            # Update the nodegroup with the desired config
+            response = eks_client.update_nodegroup_config(
+                clusterName=CLUSTER_NAME,
+                nodegroupName=nodegroup_name,
+                scalingConfig=scaling_config
+            )
+            # Yield info about the update for tracking purposes
+            yield {
+                "cluster": CLUSTER_NAME,
+                "nodegroup": nodegroup_name,
+                "update_response": response
+            }
 
-def main():
-    # https://stackoverflow.com/questions/60455830/can-you-have-an-async-handler-in-lambda-python-3-6
-    body = asyncio.get_event_loop().run_until_complete(scale_nodegroups())
-    print(json.dumps(body, indent=2, default=str))
+def lambda_handler(event, context):
+    # Return responses from the update requests so we can
+    # look up update_ids later if necessary for debugging
+    body = [r for r in scale_nodegroups()]
     return {
         "statusCode": 200,
         "body": json.dumps(body, default=str),
     }
-    
-main()
