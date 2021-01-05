e7ba79c55c24a951e1386378c3b519c2e77cf4de
Thomas Maschler <tmaschler@wri.org>
2021-01-05 13:56:33 -0500

    use random id instead of instance type for node group name

--- terraform/modules/node_group/main.tf
+++ terraform/modules/node_group/main.tf
@@ -1,6 +1,17 @@
+resource "random_id" "eks-node-group" {
+  keepers = {
+    # Generate a new id each time we make changes to the instance type
+    # In case TF gets stuck during apply, we can taint this resource to force TF
+    # to create a new random ID
+    # `terraform taint module.core-node-group.random_id.eks_node_group`
+    instance_types = var.instance_types
+  }
+  byte_length = 8
+}
+
 resource "aws_eks_node_group" "eks-node-group" {
   cluster_name    = var.cluster_name
-  node_group_name = replace("${var.node_group_name}-${var.instance_types}", ".", "_")
+  node_group_name = "${var.node_group_name}-${random_id.eks-node-group.hex}"
   node_role_arn   = var.node_role_arn
   subnet_ids      = var.subnet_ids
 
@@ -10,7 +21,7 @@ resource "aws_eks_node_group" "eks-node-group" {
     min_size     = var.min_size
   }
 
-  instance_types = [var.instance_types]
+  instance_types = [random_id.eks-node-group.keepers.instance_types]
   disk_size      = var.instance_disk_size
 
   labels = var.labels
