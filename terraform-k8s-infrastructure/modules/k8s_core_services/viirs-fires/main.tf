a6f86ba5ecbf7d607b594dcfff31a6a2497bbf93
Daniel Fernández Rico <deferrico@gmail.com>
2021-02-11 16:55:16 +0100

    Resources for viirs v1 and v2

--- terraform-k8s-infrastructure/modules/k8s_core_services/viirs-fires/main.tf
+++ terraform-k8s-infrastructure/modules/k8s_core_services/viirs-fires/main.tf
@@ -49,131 +49,146 @@ resource "aws_api_gateway_vpc_link" "viirs_fires_lb_vpc_link" {
   }
 }
 
-resource "aws_api_gateway_resource" "viirs_fires_resource" {
+resource "aws_api_gateway_resource" "viirs_fires_v2_resource" {
   rest_api_id = var.api_gateway.id
-  parent_id   = var.resource_root_id
+  parent_id   = var.resource_root_v2_id
   path_part   = "viirs-active-fires"
 }
 
-# Access by GADM levels
-resource "aws_api_gateway_resource" "viirs_fires_admin_resource" {
+resource "aws_api_gateway_resource" "viirs_latest_v2_resource" {
   rest_api_id = var.api_gateway.id
-  parent_id   = aws_api_gateway_resource.viirs_fires_resource.id
+  parent_id   = aws_api_gateway_resource.viirs_fires_v2_resource.id
+  path_part   = "latest"
+}
+
+# Access by GADM levels
+resource "aws_api_gateway_resource" "viirs_fires_admin_v2_resource" {
+  rest_api_id = var.api_gateway.id
+  parent_id   = aws_api_gateway_resource.viirs_fires_v2_resource.id
   path_part   = "admin"
 }
 
-resource "aws_api_gateway_resource" "viirs_fires_by_iso_resource" {
+resource "aws_api_gateway_resource" "viirs_fires_by_iso_v2_resource" {
   rest_api_id = var.api_gateway.id
-  parent_id   = aws_api_gateway_resource.viirs_fires_admin_resource.id
+  parent_id   = aws_api_gateway_resource.viirs_fires_admin_v2_resource.id
   path_part   = "{iso}"
 }
 
-resource "aws_api_gateway_resource" "viirs_fires_by_id1_resource" {
+resource "aws_api_gateway_resource" "viirs_fires_by_id1_v2_resource" {
   rest_api_id = var.api_gateway.id
-  parent_id   = aws_api_gateway_resource.viirs_fires_by_iso_resource.id
+  parent_id   = aws_api_gateway_resource.viirs_fires_by_iso_v2_resource.id
   path_part   = "{id1}"
 }
 
-resource "aws_api_gateway_resource" "viirs_fires_by_id2_resource" {
+resource "aws_api_gateway_resource" "viirs_fires_by_id2_v2_resource" {
   rest_api_id = var.api_gateway.id
-  parent_id   = aws_api_gateway_resource.viirs_fires_by_id1_resource.id
+  parent_id   = aws_api_gateway_resource.viirs_fires_by_id1_v2_resource.id
   path_part   = "{id2}"
 }
 
 # Access by area id
-resource "aws_api_gateway_resource" "viirs_fires_use_resource" {
+resource "aws_api_gateway_resource" "viirs_fires_use_v2_resource" {
   rest_api_id = var.api_gateway.id
-  parent_id   = aws_api_gateway_resource.viirs_fires_resource.id
+  parent_id   = aws_api_gateway_resource.viirs_fires_v2_resource.id
   path_part   = "use"
 }
 
-resource "aws_api_gateway_resource" "viirs_fires_use_by_name_resource" {
+resource "aws_api_gateway_resource" "viirs_fires_use_by_name_v2_resource" {
   rest_api_id = var.api_gateway.id
-  parent_id   = aws_api_gateway_resource.viirs_fires_use_resource.id
+  parent_id   = aws_api_gateway_resource.viirs_fires_use_v2_resource.id
   path_part   = "{name}"
 }
 
-resource "aws_api_gateway_resource" "viirs_fires_use_by_id_resource" {
+resource "aws_api_gateway_resource" "viirs_fires_use_by_id_v2_resource" {
   rest_api_id = var.api_gateway.id
-  parent_id   = aws_api_gateway_resource.viirs_fires_use_by_name_resource.id
+  parent_id   = aws_api_gateway_resource.viirs_fires_use_by_name_v2_resource.id
   path_part   = "{id}"
 }
 
 # Access Protected Areas by id
-resource "aws_api_gateway_resource" "viirs_fires_wdpa_resource" {
+resource "aws_api_gateway_resource" "viirs_fires_wdpa_v2_resource" {
   rest_api_id = var.api_gateway.id
-  parent_id   = aws_api_gateway_resource.viirs_fires_resource.id
+  parent_id   = aws_api_gateway_resource.viirs_fires_v2_resource.id
   path_part   = "wdpa"
 }
 
-resource "aws_api_gateway_resource" "viirs_fires_wdpa_by_id_resource" {
+resource "aws_api_gateway_resource" "viirs_fires_wdpa_by_id_v2_resource" {
   rest_api_id = var.api_gateway.id
-  parent_id   = aws_api_gateway_resource.viirs_fires_wdpa_resource.id
+  parent_id   = aws_api_gateway_resource.viirs_fires_wdpa_v2_resource.id
   path_part   = "{id}"
 }
 
-resource "aws_api_gateway_resource" "dataset_find_by_ids_resource" {
+# v1 resources
+resource "aws_api_gateway_resource" "viirs_fires_v1_resource" {
   rest_api_id = var.api_gateway.id
-  parent_id   = aws_api_gateway_resource.dataset_resource.id
-  path_part   = "find-by-ids"
+  parent_id   = var.resource_root_v1_id
+  path_part   = "viirs-active-fires"
 }
 
-resource "aws_api_gateway_resource" "dataset_upload_resource" {
+resource "aws_api_gateway_resource" "viirs_latest_v1_resource" {
   rest_api_id = var.api_gateway.id
-  parent_id   = aws_api_gateway_resource.dataset_resource.id
-  path_part   = "upload"
+  parent_id   = aws_api_gateway_resource.viirs_fires_v1_resource.id
+  path_part   = "latest"
 }
 
-resource "aws_api_gateway_resource" "dataset_clone_resource" {
+# Access by GADM levels
+resource "aws_api_gateway_resource" "viirs_fires_admin_v1_resource" {
   rest_api_id = var.api_gateway.id
-  parent_id   = aws_api_gateway_resource.dataset_id_resource.id
-  path_part   = "clone"
+  parent_id   = aws_api_gateway_resource.viirs_fires_v1_resource.id
+  path_part   = "admin"
 }
 
-resource "aws_api_gateway_resource" "dataset_flush_resource" {
+resource "aws_api_gateway_resource" "viirs_fires_by_iso_v1_resource" {
   rest_api_id = var.api_gateway.id
-  parent_id   = aws_api_gateway_resource.dataset_id_resource.id
-  path_part   = "flush"
+  parent_id   = aws_api_gateway_resource.viirs_fires_admin_v1_resource.id
+  path_part   = "{iso}"
 }
 
-resource "aws_api_gateway_resource" "dataset_recover_resource" {
+resource "aws_api_gateway_resource" "viirs_fires_by_id1_v1_resource" {
   rest_api_id = var.api_gateway.id
-  parent_id   = aws_api_gateway_resource.dataset_id_resource.id
-  path_part   = "recover"
+  parent_id   = aws_api_gateway_resource.viirs_fires_by_iso_v1_resource.id
+  path_part   = "{id1}"
 }
 
-resource "aws_api_gateway_resource" "dataset_last_updated_resource" {
+resource "aws_api_gateway_resource" "viirs_fires_by_id2_v1_resource" {
   rest_api_id = var.api_gateway.id
-  parent_id   = aws_api_gateway_resource.dataset_id_resource.id
-  path_part   = "lastUpdated"
+  parent_id   = aws_api_gateway_resource.viirs_fires_by_id1_v1_resource.id
+  path_part   = "{id2}"
 }
 
-module "dataset_get" {
-  source       = "../endpoint"
-  api_gateway  = var.api_gateway
-  api_resource = aws_api_gateway_resource.dataset_resource
-  method       = "GET"
-  uri          = "http://api.resourcewatch.org/api/v1/dataset"
-  vpc_link     = aws_api_gateway_vpc_link.dataset_lb_vpc_link
+# Access by area id
+resource "aws_api_gateway_resource" "viirs_fires_use_v1_resource" {
+  rest_api_id = var.api_gateway.id
+  parent_id   = aws_api_gateway_resource.viirs_fires_v1_resource.id
+  path_part   = "use"
 }
 
-module "dataset_get_by_id" {
-  source       = "../endpoint"
-  api_gateway  = var.api_gateway
-  api_resource = aws_api_gateway_resource.dataset_id_resource
-  method       = "GET"
-  uri          = "http://api.resourcewatch.org/api/v1/dataset/{id}"
-  vpc_link     = aws_api_gateway_vpc_link.dataset_lb_vpc_link
+resource "aws_api_gateway_resource" "viirs_fires_use_by_name_v1_resource" {
+  rest_api_id = var.api_gateway.id
+  parent_id   = aws_api_gateway_resource.viirs_fires_use_v1_resource.id
+  path_part   = "{name}"
 }
 
-module "dataset_clone" {
-  source       = "../endpoint"
-  api_gateway  = var.api_gateway
-  api_resource = aws_api_gateway_resource.dataset_clone_resource
-  method       = "POST"
-  uri          = "http://api.resourcewatch.org/api/v1/dataset/{id}/clone"
-  vpc_link     = aws_api_gateway_vpc_link.dataset_lb_vpc_link
+resource "aws_api_gateway_resource" "viirs_fires_use_by_id_v1_resource" {
+  rest_api_id = var.api_gateway.id
+  parent_id   = aws_api_gateway_resource.viirs_fires_use_by_name_v1_resource.id
+  path_part   = "{id}"
 }
+
+# Access Protected Areas by id
+resource "aws_api_gateway_resource" "viirs_fires_wdpa_v1_resource" {
+  rest_api_id = var.api_gateway.id
+  parent_id   = aws_api_gateway_resource.viirs_fires_v1_resource.id
+  path_part   = "wdpa"
+}
+
+resource "aws_api_gateway_resource" "viirs_fires_wdpa_by_id_v1_resource" {
+  rest_api_id = var.api_gateway.id
+  parent_id   = aws_api_gateway_resource.viirs_fires_wdpa_v1_resource.id
+  path_part   = "{id}"
+}
+
+
 
 module "dataset_flush" {
   source       = "../endpoint"
