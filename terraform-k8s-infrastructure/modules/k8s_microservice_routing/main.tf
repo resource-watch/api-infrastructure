a5142ab87ee638fd4e6e16915adea4bce321e7d4
David Inga <david.inga@vizzuality.com>
2021-02-22 13:02:49 +0100

    added aqueduct analysis ms to terraform infraestructure

--- terraform-k8s-infrastructure/modules/k8s_microservice_routing/main.tf
+++ terraform-k8s-infrastructure/modules/k8s_microservice_routing/main.tf
@@ -283,6 +283,23 @@ module "area" {
   eks_asg_names    = data.aws_autoscaling_groups.eks_autoscaling_groups.names
 }
 
+module "aqueduct-analysis" {
+  source           = "./aqueduct-analysis"
+  api_gateway      = aws_api_gateway_rest_api.rw_api_gateway
+  cluster_ca       = var.cluster_ca
+  cluster_endpoint = var.cluster_endpoint
+  cluster_name     = var.cluster_name
+  load_balancer    = aws_lb.api_gateway_nlb
+  vpc              = var.vpc
+  vpc_link         = aws_api_gateway_vpc_link.rw_api_lb_vpc_link
+  eks_asg_names    = data.aws_autoscaling_groups.eks_autoscaling_groups.names
+
+  depends_on = [
+    module.dataset,
+    module.query,
+  ]
+}
+
 module "arcgis" {
   source           = "./arcgis"
   api_gateway      = aws_api_gateway_rest_api.rw_api_gateway
