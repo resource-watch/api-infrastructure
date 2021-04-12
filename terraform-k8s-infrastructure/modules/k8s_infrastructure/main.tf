070df7037d05d59cd139fb1a70c82e6eeba6ee2b
Tiago Garcia <tiagojsag@gmail.com>
2021-04-12 10:14:02 +0200

    Fix issues with infrastructure deployment and multi-resource yaml files

--- terraform-k8s-infrastructure/modules/k8s_infrastructure/main.tf
+++ terraform-k8s-infrastructure/modules/k8s_infrastructure/main.tf
@@ -13,8 +13,13 @@ provider "kubectl" {
 // ALB Ingress Controller v1.1.8
 // RBAC roles file is as-is
 // Main file has changes - see link above for details
+data "kubectl_path_documents" "alb_ingress_controller_rbac_role_manifests" {
+  pattern = "${path.module}/alb_ingress/rbac-role.yaml"
+}
+
 resource "kubectl_manifest" "alb_ingress_controller_rbac_role" {
-  yaml_body = file("${path.module}/alb_ingress/rbac-role.yaml")
+  count     = length(data.kubectl_path_documents.alb_ingress_controller_rbac_role_manifests.documents)
+  yaml_body = element(data.kubectl_path_documents.alb_ingress_controller_rbac_role_manifests.documents, count.index)
 }
 
 resource "kubectl_manifest" "alb_ingress_controller_main" {
@@ -28,25 +33,42 @@ resource "kubectl_manifest" "alb_ingress_controller_main" {
 // https://docs.aws.amazon.com/eks/latest/userguide/cluster-autoscaler.html
 // AWS Cluster autoscaler
 // File has changes - see link above for details
-resource "kubectl_manifest" "cluster_autoscaler" {
-  yaml_body = templatefile("${path.module}/cluster_autoscaler/cluster-autoscaler-autodiscover.yaml.tmpl", {
+data "kubectl_path_documents" "cluster_autoscaler_manifests" {
+  pattern = "${path.module}/cluster_autoscaler/cluster-autoscaler-autodiscover.yaml.tmpl"
+  vars = {
     cluster_name : var.cluster_name
-  })
+  }
+}
+
+resource "kubectl_manifest" "cluster_autoscaler" {
+  count     = length(data.kubectl_path_documents.cluster_autoscaler_manifests.documents)
+  yaml_body = element(data.kubectl_path_documents.cluster_autoscaler_manifests.documents, count.index)
 }
 
 // https://docs.aws.amazon.com/eks/latest/userguide/metrics-server.html
 // AWS Metrics server for HPA support
 // File has no changes
-resource "kubectl_manifest" "metrics-server" {
-  yaml_body = file("${path.module}/metrics_server/metrics_server.yaml")
+data "kubectl_path_documents" "metrics_server_manifests" {
+  pattern = "${path.module}/metrics_server/metrics_server.yaml"
+}
+
+resource "kubectl_manifest" "metrics_server" {
+  count     = length(data.kubectl_path_documents.metrics_server_manifests.documents)
+  yaml_body = element(data.kubectl_path_documents.metrics_server_manifests.documents, count.index)
 }
 
 // https://docs.aws.amazon.com/AmazonCloudWatch/latest/monitoring/Container-Insights-setup-EKS-quickstart.html
 // Container insights
 // File has changes - see link above for details
-resource "kubectl_manifest" "container_insights" {
-  yaml_body = templatefile("${path.module}/container_insights/container_insights.yaml.tmpl", {
+data "kubectl_path_documents" "container_insights_manifests" {
+  pattern = "${path.module}/container_insights/container_insights.yaml.tmpl"
+  vars = {
     aws_region : var.aws_region,
     cluster_name : var.cluster_name
-  })
+  }
+}
+
+resource "kubectl_manifest" "container_insights" {
+  count     = length(data.kubectl_path_documents.container_insights_manifests.documents)
+  yaml_body = element(data.kubectl_path_documents.container_insights_manifests.documents, count.index)
 }
