aac7d99f009c78f0a5bc49ffa99b846ec3ae1e11
Tiago Garcia <tiagojsag@gmail.com>
2021-12-15 08:59:09 +0100

    Fix issue in AQ bucket CORS settings

--- terraform/modules/aq_bucket/main.tf
+++ terraform/modules/aq_bucket/main.tf
@@ -12,7 +12,7 @@ resource "aws_s3_bucket" "aq_bucket" {
 
   cors_rule {
     allowed_headers = ["*"]
-    allowed_methods = ["PUT", "POST"]
+    allowed_methods = ["GET"]
     allowed_origins = [var.cors_allowed_origin]
     expose_headers  = ["ETag"]
     max_age_seconds = 3000
