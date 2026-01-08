22b12389b68ad4184704a3ec66bddf6a663f874e
Anaue Curi <anaue.curi@wriconsultant.org>
2026-01-08 16:31:48 -0300

    set undo comments

--- terraform/modules/node_group/main.tf
+++ terraform/modules/node_group/main.tf
@@ -15,7 +15,7 @@ resource "aws_eks_node_group" "eks-node-group" {
   node_group_name = "${var.node_group_name}-${random_id.eks-node-group.hex}"
   node_role_arn   = var.node_role_arn
   subnet_ids      = var.subnet_ids
-#  release_version = var.eks_node_release_version
+  release_version = var.eks_node_release_version
   capacity_type   = var.capacity_type
 
   scaling_config {
